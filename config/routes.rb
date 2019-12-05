cd81794b56835d9ceb1e50ec54dabb13a313997b
github username <github email address>
2019-12-04 20:13:34 -0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  delete '/logout' => 'sessions#destroy'
+  
   resources :reviews
   resources :cookies
   resources :brands
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+root to: 'sessions#welcome'
 end
