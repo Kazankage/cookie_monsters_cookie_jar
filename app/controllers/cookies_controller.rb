652ce496c5b6178e65398acb28c5ae817bad3256
github username <github email address>
2019-12-04 20:49:49 -0800

    cookie params

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,5 +1,17 @@
 class CookiesController < ApplicationController
     def new
         @cookie = Cookie.new
+        @cookie.build_brand
     end
+
+    def create
+
+    end
+    
+
+    private 
+    def cookie_params
+        params.require(:cookie).permit(:description, :brand_id, brand_attributes: [:name])
+      end
 end
+
