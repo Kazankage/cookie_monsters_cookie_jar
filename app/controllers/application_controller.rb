c40742ca76e709f26b72b6a3adcf36455fddff0c
github username <github email address>
2019-12-06 09:35:09 -0800

    reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
-    def current_user
-        User.find_by(:username => session[:username]) if session[:username]
-         #User.find(session[:username])
-       end
 end
+
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,10 @@
 class ReviewsController < ApplicationController
     def new
+       @cookie = Cookie.find_by_id(params[:id])
+       @review = @cookie.build_review
     end
 
     def index
     end
-    
+
 end
