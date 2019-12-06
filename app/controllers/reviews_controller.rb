6f81083a49094e51ef5e983cba6da2fa625dc17c
github username <github email address>
2019-12-06 10:45:58 -0800

    typo

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
     def new
-       if @cookie = Cookie.find_by_id(params[:cooky_id])
+       if @cookie = Cookie.find_by_id(params[:cookie_id])
             @review = @cookie.reviews.build
        else
             @review = Review.new
@@ -12,7 +12,7 @@ class ReviewsController < ApplicationController
 
     def create
         @review = current_user.reviews.build(review_params)
-        if review.save!
+        if @review.save!
             redirect_to review_path(@review)
         else
          render :new
@@ -27,7 +27,7 @@ class ReviewsController < ApplicationController
 private
 
 def review_params
-    params.require(:review).permit(:ice_cream_id, :content, :stars, :title)
+    params.require(:review).permit(:cookie_id, :content, :rating, :title)
 end
 
 end
