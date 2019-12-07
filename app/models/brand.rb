3a89b5f40ae9573a5d5302b806495bc73d0d4b63
github username <github email address>
2019-12-06 21:09:06 -0800

    fixing errors

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,16 +1,11 @@
 class Brand < ApplicationRecord
     has_many :cookies
     validates :name, presence: true, uniqueness: true
-    validate :no_copycats
+ 
 
     def self.alpha
         order(:name)
-      end
-       
-    def no_copycats
-        cookie = Cookie.find_by(flavour: name, brand_id: brand_id)
-      if !!cookie && cookie != self
-        errors.add(:brand, 'is already in cookie jar!')
-      end
     end
+       
+    
 end
