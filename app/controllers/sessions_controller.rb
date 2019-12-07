d702d93747c6ff1daaf9b9ef1c867e06b98c35d4
github username <github email address>
2019-12-07 10:58:16 -0800

    fixing omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,12 +12,7 @@ class SessionsController < ApplicationController
     end
 
     def create 
-      if params[:provider] == 'google_oauth2'
-      @user = User.create_by_google_omniauth(auth)
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
-
-      else
+  
         @user = User.find_by(username: params[:user][:username])
 
         if @user.try(:authenticate, params[:user][:password])
@@ -28,22 +23,23 @@ class SessionsController < ApplicationController
           flash[:error] = "Sorry, login info was incorrect. Please try again."
           redirect_to login_path
         end
-    end
+    
   end
 
-    def omniauth
-      @user = User.create_by_google_omniauth(auth)
-      session[:user_id] = @user.id
-      session[:user_username] = @user.username
-      session[:user_email] = @user.email
-    
-      redirect_to user_path(:user)
-      
+
+  def omniauth
+    byebug
+    @user = User.find_or_create_by(email: auth[:info][:email]) do |pass|
+      pass.password = SecureRandom.hex
+      pass.username = pass.email
     end
-  
-    private
-  
-      def auth
-        request.env['omniauth.auth']
-      end
+    sessions[:id] = @user.id
+    #User.where(email: auth[:info][:email]).first_or_initialize     
   end
+
+  private
+
+    def auth
+      request.env["omniauth.auth"]
+    end
+end
